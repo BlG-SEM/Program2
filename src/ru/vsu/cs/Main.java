a4c76a7a70473434837af8199f717d9284d7375f
BlG-SEM <71176268+BlG-SEM@users.noreply.github.com>
2020-11-02 10:57:42 +0300

    Update Main.java

--- src/ru/vsu/cs/Main.java
+++ src/ru/vsu/cs/Main.java
@@ -4,30 +4,30 @@ import java.util.Scanner;
 import java.util.Locale;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Enter numbers А, В, С");
-        double a = readNumber("a = ");
-        double b = readNumber("b = ");
-        double c = readNumber("c = ");
-        sortNumberInAscendingOrder(a, b, c);
+     public static void main(String[] args) {
+        double aNumber = readNumber("A = ");
+        double bNumber = readNumber("B = ");
+        double cNumber = readNumber("C = ");
+ 
+        double[] arrOfSortedNumbers = sortNumbersInAscendingOrder(aNumber, bNumber, cNumber);
+ 
+        if (isArithmeticProgression(arrOfSortedNumbers)) {
+            printPhrase("a arithmetic progression, d =",
+                    getCoefficientOfArithmeticProgression(arrOfSortedNumbers[1], arrOfSortedNumbers[0]));
+        } else if (isGeometricProgression(arrOfSortedNumbers)) {
+            printPhrase("a geometric progression, q =",
+                    getCoefficientOfGeometricProgression(arrOfSortedNumbers[1], arrOfSortedNumbers[0]));
+        } else
+            System.out.println("The sequence is not a progression");
     }
-
+ 
     private static double readNumber(String name) {
         Scanner Scanner = new Scanner(System.in);
         System.out.printf("Enter %s", name);
         return Scanner.nextDouble();
     }
-
-    private static void processProgression(double a, double b, double c) {
-        if (b + (b - a) == c) {
-            System.out.printf("It's a arithmetic progression, d = %1$.1f", b - a);
-        } else if (b * (b / a) == c) {
-            System.out.printf("It's a geometric progression, q = %1$.1f", b / a);
-        } else
-            System.out.println("The sequence is not a progression");
-    }
-
-    private static void sortNumberInAscendingOrder(double a, double b, double c) {
+ 
+    private static double[] sortNumbersInAscendingOrder(double a, double b, double c) {
         if (a > b) {
             a = a + b;
             b = a - b;
@@ -43,7 +43,30 @@ public class Main {
             b = a - b;
             a = a - b;
         }
-
-        processProgression(a, b, c);
+ 
+        return new double[]{a, b, c};
+    }
+ 
+    private static boolean isArithmeticProgression(double[] arrOfSortedNumbers) {
+        double coefficient = getCoefficientOfArithmeticProgression(arrOfSortedNumbers[1], arrOfSortedNumbers[0]);
+        return arrOfSortedNumbers[1] + coefficient == arrOfSortedNumbers[2];
+    }
+ 
+    private static boolean isGeometricProgression(double[] arrOfSortedNumbers) {
+        double coefficient = getCoefficientOfGeometricProgression(arrOfSortedNumbers[1], arrOfSortedNumbers[0]);
+        return arrOfSortedNumbers[1] * coefficient == arrOfSortedNumbers[2];
+    }
+ 
+    private static double getCoefficientOfArithmeticProgression(double firstNumber, double secondNumber) {
+        return firstNumber - secondNumber;
+    }
+ 
+    private static double getCoefficientOfGeometricProgression(double firstNumber, double secondNumber) {
+        return firstNumber / secondNumber;
+    }
+ 
+    private static void printPhrase(String phrase, double coefficient) {
+        System.out.printf("It's %s ", phrase);
+        System.out.println(coefficient);
     }
 }
