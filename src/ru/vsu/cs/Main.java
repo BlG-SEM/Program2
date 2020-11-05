f88d0d2bf20125ae88a973a24e30537acd2ba809
BlG-SEM <71176268+BlG-SEM@users.noreply.github.com>
2020-11-05 11:34:19 +0300

    Update Main.java

--- src/ru/vsu/cs/Main.java
+++ src/ru/vsu/cs/Main.java
@@ -5,40 +5,36 @@ import java.util.Locale;
 
 public class Main {
      public static void main(String[] args) {
-        double aNumber = readNumber("A = ");
-        double bNumber = readNumber("B = ");
-        double cNumber = readNumber("C = ");
+        double[] arrNumbers = new double[3];
+        readNumber(arrNumbers);
+        Arrays.sort(arrNumbers);
  
-        double[] arrOfSortedNumbers = { 
-             Math.min(Math.min(aNumber, bNumber), cNumber), 
-             Math.max(Math.min(aNumber, bNumber), Math.min(Math.max(aNumber, bNumber), cNumber)), 
-             Math.max(Math.max(aNumber, bNumber), cNumber)
-        };
- 
-        if (isArithmeticProgression(arrOfSortedNumbers)) {
+        if (isArithmeticProgression(arrNumbers)) {
             printPhrase("a arithmetic progression, d =",
-                    getCoefficientOfArithmeticProgression(arrOfSortedNumbers[1], arrOfSortedNumbers[0]));
-        } else if (isGeometricProgression(arrOfSortedNumbers)) {
+                    getCoefficientOfArithmeticProgression(arrNumbers[1], arrNumbers[0]));
+        } else if (isGeometricProgression(arrNumbers)) {
             printPhrase("a geometric progression, q =",
-                    getCoefficientOfGeometricProgression(arrOfSortedNumbers[1], arrOfSortedNumbers[0]));
+                    getCoefficientOfGeometricProgression(arrNumbers[1], arrNumbers[0]));
         } else
             System.out.println("The sequence is not a progression");
     }
  
-    private static double readNumber(String name) {
+    private static void readNumber(double[] arrNumbers) {
         Scanner Scanner = new Scanner(System.in);
-        System.out.printf("Enter %s", name);
-        return Scanner.nextDouble();
+        System.out.println("Enter number");
+        for(int i = 0; i < 3; i++) {
+             arrNumbers[i] = Scaner.nextDouble();
+        }
     }
  
-    private static boolean isArithmeticProgression(double[] arrOfSortedNumbers) {
-        double coefficient = getCoefficientOfArithmeticProgression(arrOfSortedNumbers[1], arrOfSortedNumbers[0]);
-        return arrOfSortedNumbers[1] + coefficient == arrOfSortedNumbers[2];
+    private static boolean isArithmeticProgression(double[] arrNumbers) {
+        double coefficient = getCoefficientOfArithmeticProgression(arrNumbers[1], arrNumbers[0]);
+        return arrNumbers[1] + coefficient == arrNumbers[2];
     }
  
-    private static boolean isGeometricProgression(double[] arrOfSortedNumbers) {
-        double coefficient = getCoefficientOfGeometricProgression(arrOfSortedNumbers[1], arrOfSortedNumbers[0]);
-        return arrOfSortedNumbers[1] * coefficient == arrOfSortedNumbers[2];
+    private static boolean isGeometricProgression(double[] arrNumbers) {
+        double coefficient = getCoefficientOfGeometricProgression(arrNumbers[1], arrNumbers[0]);
+        return arrNumbers[1] * coefficient == arrNumbers[2];
     }
  
     private static double getCoefficientOfArithmeticProgression(double firstNumber, double secondNumber) {
